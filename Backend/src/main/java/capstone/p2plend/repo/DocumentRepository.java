9729d3cf3bf454bfd12be89458097852834e1876
AdlerkoV <adlerkov@gmail.com>
2019-07-29 14:41:18 +0700

    Update DocumentRepository.java

--- Backend/src/main/java/capstone/p2plend/repo/DocumentRepository.java
+++ Backend/src/main/java/capstone/p2plend/repo/DocumentRepository.java
@@ -10,12 +10,12 @@ import org.springframework.data.repository.query.Param;
 
 public interface DocumentRepository extends JpaRepository<Document, Integer> {
 	
-	@Query(value = "SELECT * FROM document WHERE document_id = :status AND document_type_id = :documentTypeId", nativeQuery = true)
+	@Query(value = "SELECT * FROM document WHERE document_id = :documentId AND document_type_id = :documentTypeId", nativeQuery = true)
 	Document findByDocumentIdAndDocumentType(@Param("documentId") String documentId, @Param("documentTypeId") Integer documentTypeId);
 	
 	@Query(value = "SELECT * FROM document WHERE status = :status", nativeQuery = true)
 	Page<Document> findAllDocumentWithStatus(Pageable pageable, @Param("status") String status);
 		
 	@Query(value = "SELECT * FROM document WHERE document_type_id = :documentTypeId AND user_id = :id", nativeQuery = true)
-	Document findUserDocument(@Param("documentType") Integer documentTypeId, @Param("id") Integer id);
+	Document findUserDocument(@Param("documentTypeId") Integer documentTypeId, @Param("id") Integer id);
 }
