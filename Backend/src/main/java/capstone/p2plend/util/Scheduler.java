85101e016ac316bdb225f9b8c51f2a4e962be753
AdlerkoV <adlerkov@gmail.com>
2019-08-06 16:12:09 +0700

    Update Scheduler.java

--- Backend/src/main/java/capstone/p2plend/util/Scheduler.java
+++ Backend/src/main/java/capstone/p2plend/util/Scheduler.java
@@ -1,5 +1,7 @@
 package capstone.p2plend.util;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,29 +20,41 @@ public class Scheduler {
 
 	@Autowired
 	EmailService emailService;
-	
+
 	@Autowired
 	RequestRepository requestRepo;
-	
-//	@Scheduled(cron = "0 55 15 * * ?")
+
+//	@Scheduled(cron = "* * * * * ?")
 //	public void testScheduler() {				
-//		
+//		System.out.println("run ..");
 //	}
-	
-//	@Scheduled(cron = "0 0 1 * * ?")
-	public void testScheduler() {				
+
+	@Scheduled(cron = "0 0 4 * * ?")
+	public void sendMailScheduler() {
 		try {
+			System.out.println("Start scaning system...");
 			List<Request> lstRequest = requestRepo.findAllRequestByStatus("trading");
-			for(int i = 0; i < lstRequest.size(); i++) {
+			for (int i = 0; i < lstRequest.size(); i++) {
 				Request r = lstRequest.get(i);
 				Deal deal = r.getDeal();
 				User borrower = r.getBorrower();
 				User lender = r.getLender();
 				List<Milestone> lstMilestone = deal.getMilestone();
+				for (int x = 0; x < lstMilestone.size(); x++) {
+					Milestone m = lstMilestone.get(x);
+					if (m.getPercent() == null) {
+						continue;
+					}
+					Long time = m.getPreviousDate() * 1000;
+					Timestamp stamp = new Timestamp(time);
+					Date date = new Date(stamp.getTime());
+					Date currentDate = new Date();
+					date.before(currentDate);
+				}
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 	}
-	
+
 }
